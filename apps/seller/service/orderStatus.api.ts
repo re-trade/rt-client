287c1efe4c3a4c9aa99d5ccd807e58ea8b2e1118
tohka-vo-tri <imchibitouka@gmail.com>
2025-07-10 22:02:49 +0700

    change

--- apps/seller/service/orderStatus.api.ts
+++ apps/seller/service/orderStatus.api.ts
@@ -1,21 +1,20 @@
 import { authApi, IResponseObject } from '@retrade/util';
 export type OrderStatusResponse = {
-    id: string;
-    name: string;
-    code: string;
-    };
+  id: string;
+  name: string;
+  code: string;
+};
 
 export const orderStatusApi = {
-    async getAllOrderStatuses(): Promise<OrderStatusResponse[]> {
-        const response = await authApi.default.get<IResponseObject<OrderStatusResponse[]>>(
-            `/orders/status`
-        );
-        return response.data.success ? response.data.content : [];
-    },
-    async getOrderStatusById(id: string): Promise<OrderStatusResponse | null> {
-        const response = await authApi.default.get<IResponseObject<OrderStatusResponse>>(
-            `/orders/status/${id}`
-        );
-        return response.data.success ? response.data.content : null;
-    },
-}
+  async getAllOrderStatuses(): Promise<OrderStatusResponse[]> {
+    const response =
+      await authApi.default.get<IResponseObject<OrderStatusResponse[]>>(`/orders/status`);
+    return response.data.success ? response.data.content : [];
+  },
+  async getOrderStatusById(id: string): Promise<OrderStatusResponse | null> {
+    const response = await authApi.default.get<IResponseObject<OrderStatusResponse>>(
+      `/orders/status/${id}`,
+    );
+    return response.data.success ? response.data.content : null;
+  },
+};
