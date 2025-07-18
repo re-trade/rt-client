5f326f3b9f2f8c245b00346915c239c0162be003
AnataAria <arisavinh2003@gmail.com>
2025-07-18 15:48:37 +0700

    misc: clean code

--- apps/admin/app/dashboard/layout.tsx
+++ apps/admin/app/dashboard/layout.tsx
@@ -17,7 +17,6 @@ import {
 import {
   AlertCircle,
   Banknote,
-  BarChart3,
   DollarSign,
   FileText,
   Flag,
@@ -46,7 +45,6 @@ const menuItems = [
       { title: 'Quản lý người bán', icon: Store, href: '/dashboard/seller' },
       { title: 'Quản lý tố cáo', icon: Flag, href: '/dashboard/report-seller' },
       { title: 'Quản lý Category', icon: FileText, href: '/dashboard/category' },
-
     ],
   },
   {
@@ -55,7 +53,6 @@ const menuItems = [
       { title: 'Doanh thu hệ thống', icon: DollarSign, href: '/dashboard/revenue' },
       { title: 'Báo cáo tài chính', icon: FileText, href: '/dashboard/financial' },
       { title: 'Yêu cầu rút tiền', icon: Banknote, href: '/dashboard/withdraw' },
-
     ],
   },
   {
